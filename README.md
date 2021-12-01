640af72f965b2bfbabb2f8ad15fd498e3472862e
orangeboyChen <chenenhan@qq.com>
2021-12-01 16:46:31 +0800

    update:README.md
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    update: 添加部分页面
    
    update: 添加部分页面
    
    update: 添加主页readme
    
    bugfix:修复了文件夹命名错误
    
    update: 添加部分内容
    
    update: 修复部分问题
    
    update: 修复部分问题
    
    update: 修改了部分页面
    
    update: 添加favicon
    
    update: 修改部分地址
    
    update: 修改了更新网址
    
    bugfix:修复了无法查看testflight版本的bug
    
    bugfix:修复了无法查看testflight版本的bug
    
    bugfix:修复了无法查看testflight版本的bug2
    
    bugfix:修复了打包失败的问题
    
    bugfix:修复了无效引用的问题
    
    Update library.md
    
    Add 自动化（iOS）explanation.
    
    Update README.md
    
    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# whu-ham
-武汉大学生活助手——课表查询、跑步、预约图书馆
+
+<p align="center"><img src="./docs/.vuepress/public/icon-1024.png" width="18%" alt="Ham的Logo"/></p>
+
+<div align="center">
+<h1>Ham</h1>
+<span>查询课表、成绩、跑步、预约武汉大学图书馆</span><br><br>
+<a href="https://gitter.im/whu-ham/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge"><img src="https://img.shields.io/badge/chat-on%20gitter-28BC99.svg" alt="Chat on gitter"></a>
+</div><br>
+
+
+
+### 文档
+需要获取使用帮助请[前往文档主页](https://ham.nowcent.cn)
+
+**注意：如果Android 1.2.4无法安装，可尝试下载[这个](https://ham-android-archive.oss-cn-shenzhen.aliyuncs.com/ham-1.2.4.apk)**
+
+
+### 下载安装
+[前往下载地址(Android / iOS)](https://orangeboychen.github.io/whu-ham/download/)
+
+
+### 它能做什么
+
+#### 课程表
+- 导入武汉大学课程表
+- 自定义课程
+- 课程提醒
+
+#### 成绩
+- 查询成绩、计算GPA、综测成绩
+- 还没有啥数据的查给分
+
+#### 图书馆
+- 按照条件筛选、预约图书馆
+- 每日自动预约图书馆
+- 更改图书馆预约时间
+
+~~跑步~~
+
+~~- 制定跑步计划~~（该功能暂时下线，正在适配绿色的APP）
+
+#### 其它
+- 课程表、图书馆预约小组件支持（Android、iOS均可）
+
+
+### 它安全吗
+
+放心，Ham并不会像某些已经停止服务的小程序一样，将你的学号密码发送到他们的服务器，再进行验证。你的账户信息仅保存在本地，你的手机将直接从学校服务器获取数据，而不会通过第三方转发。
+
+当然了，Ham有查给分等功能，需要用到后台服务器。但请放心，Ham在传输你的信息时，务必通过你的同意。而且Ham不会直接传输关键信息（学号、密码等）。你的关键信息（学号等）在进行哈希运算后再传输至服务器，这样我在后台也不知道是谁的成绩。
---
+++ docs/.vuepress/components/iOSDownloadLink.vue
@@ -0,0 +1,133 @@
+<template>
+  <div>
+    <div v-if="finalRelease.name !== ''">
+      <h3 style="margin-bottom: 2px;">正式版 {{finalRelease.name}}</h3>
+      <span class="caption">发布于 {{formatDate(finalRelease.publishDate)}}</span>
+
+      <blockquote>
+        <h4 style="margin-bottom: 4px;margin-top: 8px;">更新日志</h4>
+        <div v-html="finalRelease.updateLog.replace(/新增内容：\n/g, '').replace(/· /g, '').replace(/\n/g, '<br>')"></div>
+      </blockquote>
+    </div>
+    <div class="link">
+      <a href="https://apps.apple.com/cn/app/ham/id1577896044" target="_blank" rel="noopener noreferrer">
+        前往App Store下载正式版
+        <OutboundLink/>
+      </a>
+
+    </div>
+    <div v-if="betaRelease.name !== ''">
+      <h3 style="margin-bottom: 2px;">测试版 {{betaRelease.name}}</h3>
+      <span v-if="betaRelease.publishDate != null" class="caption">发布于 {{formatDate(betaRelease.publishDate)}}</span>
+      <blockquote v-if="betaRelease.updateLog !== ''">
+        <h4 style="margin-bottom: 4px;margin-top: 8px;">更新日志</h4>
+        <div v-html="betaRelease.updateLog.replace(/- /g, '').replace(/\n/g, '<br>')"></div>
+      </blockquote>
+    </div>
+    <div class="custom-block tip"  style="border-color: lightgray;">
+      <p class="custom-block-title">提示</p>
+      <p>下载测试版前，请确保你的设备已安装
+        <a href="https://apps.apple.com/us/app/testflight/id899247664" target="_blank" rel="noopener noreferrer">
+        TestFlight
+        <OutboundLink/>
+      </a>
+        。
+      </p>
+    </div>
+    <div class="link">
+<!--      https://testflight.apple.com/join/waKNnCG3-->
+      <a href="itms-beta://testflight.apple.com/join/waKNnCG3" target="_blank" rel="noopener noreferrer">
+        加入TestFlight内测
+        <OutboundLink/>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import moment from 'moment'
+
+export default {
+  name: "iOSDownloadLink",
+  data() {
+    return {
+      finalRelease: {
+        name: '',
+        updateLog: '',
+        publishDate: new Date(),
+
+      },
+      betaRelease: {
+        name: '',
+        updateLog: '',
+        publishDate: new Date(),
+      }
+    }
+  },
+  created() {
+    this.getIOSReleaseVersionInfo();
+    this.getIOSBetaVersionInfo();
+  },
+  methods: {
+    formatDate: function(date) {
+      moment.locale('zh-cn');
+      return moment(date).fromNow();
+    },
+    getIOSReleaseVersionInfo: async function () {
+      // let res = await axios.get('https://itunes.apple.com/lookup?id=1577896044', {
+      //   headers: {
+      //     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15'
+      //   }
+      // });
+      // if(res.status !== 200 || res.data.resultCount <= 0) {
+      //   return;
+      // }
+
+      // 得到查询结果后的回调函数;
+
+      if(typeof window === 'undefined') {
+        return;
+      }
+
+      let that = this;
+      window.jsonpCallback = function (data) {
+        let releaseReleaseInfo = data.results[0];
+
+        //组装数据
+        that.finalRelease.name = releaseReleaseInfo.version;
+        that.finalRelease.updateLog = releaseReleaseInfo.releaseNotes;
+        that.finalRelease.publishDate = new Date(releaseReleaseInfo.releaseDate);
+      };
+
+      let url = "https://itunes.apple.com/lookup?id=1577896044&callback=jsonpCallback";
+      let script = document.createElement("script");
+      script.setAttribute("src", url);
+      document.getElementsByTagName("head")[0].appendChild(script);
+    },
+    getIOSBetaVersionInfo: async function () {
+      let res = await axios.get('https://api.ham.nowcent.cn/version');
+      if(res.status !== 200) {
+        return;
+      }
+
+      let betaReleaseInfo = res.data.data.iosVersion;
+      this.betaRelease.name = `${betaReleaseInfo.versionName}(${betaReleaseInfo.versionCode})`
+      this.betaRelease.updateLog = betaReleaseInfo.versionDescription ?? '';
+      this.betaRelease.publishDate = betaReleaseInfo.betaUpdateTime ?? null;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.caption {
+  color: gray;
+  font-size: small;
+}
+
+.link {
+  margin-top: 16px;
+}
+
+</style>
